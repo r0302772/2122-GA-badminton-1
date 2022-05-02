f0a00fd4b0ced459bf6a00d099eea075a66421a0
NR0893 <reniersniels@hotmail.com>
2022-05-02 20:35:51 +0200

    Basisklasse

--- Badminton_DAL/BaseModels/BasisKlasse.cs
+++ Badminton_DAL/BaseModels/BasisKlasse.cs
@@ -7,8 +7,41 @@ using System.ComponentModel;
 
 namespace Badminton_DAL.BaseModels
 {
-    public abstract class BasisKlasse : INotifyPropertyChanged
+    public abstract class BasisKlasse : INotifyPropertyChanged,IDataErrorInfo
     {
+        public abstract string this[string columnName] { get; }
+
+        
+
         public event PropertyChangedEventHandler PropertyChanged;
+        public bool IsGeldig()
+        {
+            return string.IsNullOrWhiteSpace(Error);
+
+        }
+
+        public string Error
+        {
+
+            get
+            {
+                string foutmeldingen = "";
+
+                foreach (var item in this.GetType().GetProperties())
+                {
+                    if (item.CanRead)
+                    {
+                        string fout = this[item.Name];
+                        if (!string.IsNullOrWhiteSpace(fout))
+                        {
+                            foutmeldingen += fout + Environment.NewLine;
+                        }
+                    }
+                }
+                return foutmeldingen;
+            }
+        }
+
+
     }
 }
