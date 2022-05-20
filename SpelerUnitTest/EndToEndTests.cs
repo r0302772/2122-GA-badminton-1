3ca822ea3464a429da7c445663c243446f5184c5
fofokseek <dennis.nijs@hotmail.com>
2022-05-20 18:14:05 +0200

    update unit tests

---
+++ SpelerUnitTest/EndToEndTests.cs
@@ -0,0 +1,117 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace SpelerUnitTest
+{
+    [TestFixture]
+    public class EndToEndTests
+    {
+        string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        string WpfAppId = @"C:\Users\Dennis Nijs\Documents\School\Programmeren Jaar 2\2e semester\Agile en testing\E2E testing\Nunit Tutorial\Nunit Tutorial\bin\Debug\Nunit Tutorial.exe";
+
+        WindowsDriver<WindowsElement> session;
+
+        WindowsElement voornaam, familienaam, geslacht, geboortedatum, telefoonnummer, email, foutmelding, toevoegen, aanpassen, verwijderen;
+
+        [SetUp]
+        public void Setup()
+        {
+            if (session == null)
+            {
+                var appiumOptions = new AppiumOptions();
+                appiumOptions.AddAdditionalCapability("app", WpfAppId);
+                session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appiumOptions);
+
+                voornaam = session.FindElementByAccessibilityId("Voornaam");
+                familienaam = session.FindElementByAccessibilityId("Familienaam");
+                geslacht = session.FindElementByAccessibilityId("Geslacht");
+                geboortedatum = session.FindElementByAccessibilityId("Geboortedatum");
+                telefoonnummer = session.FindElementByAccessibilityId("Telefoonnummer");
+                email = session.FindElementByAccessibilityId("Email");
+
+                foutmelding = session.FindElementByAccessibilityId("Foutmelding");
+
+                toevoegen = session.FindElementByAccessibilityId("Toevoegen");
+                aanpassen = session.FindElementByAccessibilityId("Aanpassen");
+                verwijderen = session.FindElementByAccessibilityId("Verwijderen");
+            }
+        }
+
+        [Test]
+        public void SpelerToevoegen()
+        {
+            // Act
+            voornaam.SendKeys("Tom");
+            familienaam.SendKeys("Waes");
+            geslacht.SendKeys("M");
+            geboortedatum.SendKeys("20/12/1970");
+            telefoonnummer.SendKeys("0477889900");
+            email.SendKeys("tom.Waes@hotmail.com");
+            toevoegen.Click();
+
+            // Assert
+            Assert.AreEqual("Tom", voornaam.Text);
+            Assert.AreEqual("Waes", familienaam.Text);
+            Assert.AreEqual("M", geslacht.Text);
+            Assert.AreEqual("20/12/1970", geboortedatum.Text);
+            Assert.AreEqual("0477889900", telefoonnummer.Text);
+            Assert.AreEqual("tom.Waes@hotmail.com", email.Text);
+            Assert.AreEqual("", foutmelding.Text);
+        }
+
+        [Test]
+        public void SpelerAanpassen()
+        {
+            // Act
+
+            aanpassen.Click();
+
+            // Assert
+
+        }
+
+        [Test]
+        public void SpelerVerwijderen()
+        {
+            // Act
+
+            verwijderen.Click();
+
+            // Assert
+
+        }
+
+        [Test]
+        public void SpelerLezen()
+        {
+            // Act
+
+
+            // Assert
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            voornaam.Clear();
+            familienaam.Clear();
+            geslacht.Clear();
+            geboortedatum.Clear();
+            telefoonnummer.Clear();
+            email.Clear();
+        }
+
+        [OneTimeTearDown]
+        public void CloseSession()
+        {
+            session.Close();
+        }
+    }
+}
---
+++ SpelerUnitTest/MockingTests.cs
@@ -0,0 +1,99 @@
+﻿using Badminton_DAL;
+using FakeItEasy;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpelerUnitTest
+{
+    [TestFixture]
+    public class MockingTests
+    {
+        private BadmintonEntities entities = A.Fake<BadmintonEntities>();
+
+        [Test]
+        public void OphalenSpelers()
+        {
+            //Arrange
+            ObservableCollection<Speler> spelers;
+
+            //Act
+            spelers = new ObservableCollection<Speler>(entities.Spelers.Local);
+
+            //Assert
+            Assert.NotNull(spelers);
+            Assert.IsInstanceOf<ObservableCollection<Speler>>(spelers);
+        }
+
+        [Test]
+        public void SpelerCreate()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        [Test]
+        public void SpelerRead()
+        {
+            //Arrange
+            Speler speler = A.Fake<Speler>();
+
+            //Act
+            speler = entities.Spelers.Find(DatabaseOperations.GetSpelerByPK(speler.Id));
+
+            //Assert
+            Assert.NotNull(speler);
+            Assert.IsInstanceOf<Speler>(speler);
+        }
+
+        [Test]
+        public void SpelerUpdate()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        [Test]
+        public void SpelerDelete()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        [Test]
+        public void OphalenClubs()
+        {
+            //Arrange
+            ObservableCollection<Club> clubs;
+
+            //Act
+            clubs = new ObservableCollection<Club>(entities.Clubs.Local);
+
+            //Assert
+            Assert.NotNull(clubs);
+            Assert.IsInstanceOf<ObservableCollection<Speler>>(clubs);
+        }
+    }
+}
