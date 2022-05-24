4280ad12c2c8cc0265336e07be89ee96590466c3
Suleymanyavas <suleymanyavas90@gmail.com>
2022-05-24 18:31:19 +0200

    personeel toevoegen aan een club

--- Badminton_WPF/ViewModels/PersoneelViewModel.cs
+++ Badminton_WPF/ViewModels/PersoneelViewModel.cs
@@ -5,11 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using Badminton_DAL;
 using System.Collections.ObjectModel;
+using Badminton_WPF.Views;
+using System.Windows;
 
 namespace Badminton_WPF.ViewModels
 {
     public class PersoneelViewModel : BasisViewModel
     {
+        public string titel = "Badminton Vlaanderen";
         #region Properties
         private Werknemer _werknemerRecord;
 
@@ -92,7 +95,7 @@ namespace Badminton_WPF.ViewModels
             Functies = new ObservableCollection<Functie>(DatabaseOperations.GetFuncties());
             Clubs = new ObservableCollection<Club>(DatabaseOperations.GetClubs());
             WerknemerRecord = new Werknemer();
-            WerknemerRecord.IsGeldig();
+            //WerknemerRecord.IsGeldig();
         }
 
         public void WerknemerRecordInstellen()
@@ -100,7 +103,7 @@ namespace Badminton_WPF.ViewModels
             if (GeselecteerdeWerknemer != null)
             {
                 WerknemerRecord = GeselecteerdeWerknemer;
-
+                
                 //Geslacht = DatabaseOperations.GetGeslachtById(int.Parse(GeselecteerdeSpeler.GeslachtID));
                 //SpelerRecord.GeslachtID = GeselecteerdeGeslacht.Id;
             }
@@ -119,6 +122,8 @@ namespace Badminton_WPF.ViewModels
             switch (parameter.ToString())
             {
                 case "Zoeken": return true;
+                case "OpenToevoegenwerknemer": return true;
+                case "Toevoegen": return true;
 
             }
             return true;
@@ -130,20 +135,56 @@ namespace Badminton_WPF.ViewModels
             switch (parameter.ToString())
             {
                 case "Zoeken": ZoekWerknemerByClub(GeselecteerdeClub); break;
-                    //case "Verwijderen": return true;
-                    //case "Toevoegen": return true;
-                    //case "Aanpassen": return true;
+                case "OpenToevoegenwerknemer": OpenToevoegenWerknemerScherm(); break;
+                case "Toevoegen": Toevoegen(); break;
             }
 
         }
 
+        public void Toevoegen()
+        {
+
+            if (WerknemerRecord.IsGeldig())
+            {
+
+
+                int ok = DatabaseOperations.WerknemerToevoegen(WerknemerRecord);
+                if (ok > 0)
+                {
+
+                    Wissen();
+                    MessageBox.Show("Werknemer succesvol toegevoegd aan club", "melding", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    Foutmelding = "Speler is niet toegevoegd!";
+                }
+            }
+
+        }
+
+        public void Wissen()
+        {
+            GeselecteerdeWerknemer = null;
+            WerknemerRecordInstellen();
+            Foutmelding = "";
+        }
+
         public void ZoekWerknemerByClub(Club club)
         {
-          
+
             Werknemers = new ObservableCollection<Werknemer>(DatabaseOperations.GetWerkenemerByClub(club));
             Clubs = new ObservableCollection<Club>(DatabaseOperations.GetClubs());
             Functies = new ObservableCollection<Functie>(DatabaseOperations.GetFuncties());
 
+        }
+
+        public void OpenToevoegenWerknemerScherm()
+        {
+            PersoneelViewModel viewmodel = new PersoneelViewModel();
+            PersoneelToevoegenView view = new PersoneelToevoegenView() { Title = $"{titel} | Werknemer toevoegen" };
+            view.DataContext = viewmodel;
+            view.Show();
         }
 
         private string _foutmelding;
