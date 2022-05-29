1815a86b0cb8dd6d4cfefbee1c24f4c0b2c339ba
l0reas <32838187+l0reas@users.noreply.github.com>
2022-05-29 17:39:12 +0200

    Updated password hash

--- StartData.sql
+++ StartData.sql
@@ -18,4 +18,4 @@ Insert into dbo.Geslachten(naam)
 Values ('X');
 
 Insert into dbo.Gebruiker(Gebruikersnaam,Wachtwoord)
-Values ('admin','fc8252c8dc55839967c58b9ad755a59b61b67c13227ddae4bd3f78a38bf394f7');
+Values ('admin','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918');
